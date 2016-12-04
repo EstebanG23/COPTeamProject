6b4cee1c4b2dcf16c373de36b69dc934fa846189
redgreene <redgreenenaoh@gmail.com>
2016-12-04 02:21:46 -0500

    Update MAC2312.h

--- MAC2312.h
+++ MAC2312.h
@@ -1,46 +1,29 @@
-#include <array>
+#include "Course.cpp"
+#ifndef _MAC2312_H_
+#define _MAC2312_H_
 
 //this bit says that this particular class is a child of Course
-class MAC2312 : private Course { //not sure if private is the thing to use here
+class MAC2312 :  public Course {
 private:
-	//NOTE: exams 60 points each, 80 point final
+	//NOTE: 3 exams 60 points each, 80 point final
 	//course grade is out of 400 points
 	double hittPoints; //35 points
 	double participation; //4 points
-	double webAssign;//50 max out of 54.5 points
-	std::array <double, 8> quizzes; //best 6 of 8, 6 points each
-	std::array <double, 3> writtenHomework; //3 collected, 5 points each
-
-	void calcGPA() {
-	
-	}
+	double webAssign;//50 max out of 54.5 points, a double because no specific number of assignments is given in syllabus
+	double quizzes[8]; //best 6 of 8, 6 points each
+	double writtenHomework[3]; //3 collected, 5 points each
 
 public:
-	void updateHittPoints(double) {
-
-	}
-	void updateWebAssign(int, double) {
-
-	}
-	void updateQuiz(int, double) {
-
-	}
-	void updateWrittenHomework(int, double) {
-
-	}
-	void updateParticipation(double) {
-
-	}
-	void updateExam(int, double) {
-
-	}
-	void updateFinal(double) {
-
-	}
-	double pointSummer(std::array) {
-
-	}
-	double bestOfQuizzes(std::array) {
-
-	}
-}
+	MAC2312::MAC2312();
+	void calcGPA();
+	void updateHittPoints(double);
+	void updateWebAssign(double);
+	void updateQuiz(int, double);
+	void updateWrittenHomework(int, double);
+	void updateParticipation(double);
+	void updateExam(int, double);
+	void updateFinal(double);
+	double pointSummer(double[], int);
+	double bestOfQuizzes(double[]);
+};
+#endif
