53ef770f6ff56eb01897378a660c4d9a56db51a4
redgreene <redgreenenaoh@gmail.com>
2016-12-04 16:05:47 -0500

    Update MAS3114.cpp

--- MAS3114.cpp
+++ MAS3114.cpp
@@ -1,19 +1,114 @@
-#include "stdafx.h"
 #include "MAS3114.h"
 
+MAS3114::MAS3114() {
+	participation = -1;
+	quiz = -1;
+	finals = -1;
+	for (int i = 0; i < 30; i++) {
+		if (i < 20) {
+			exams[i] = -1;
+		}
+		if (i < 5) {
+			projects[i] = -1;
+			tempProjects[i] = -1;
+		}
+		homework[i] = -1;
+		tempHomework[i] = -1;
+	}
+}
+//these methods take in a new grade value and change or set the existing value
+void MAS3114::updateHomework(int homeworkNumber, double newScore) {
+	homework[homeworkNumber] = newScore;
+	tempHomework[homeworkNumber] = newScore;
+}
+void MAS3114::updateProjects(int projectNumber, double newScore) {
+	projects[projectNumber] = newScore;
+	tempProjects[projectNumber] = newScore;
+}
+void MAS3114::updateParticipation(double newScore) {
+	participation = newScore;
+}
+void MAS3114::updateQuiz(double newScore) {
+	quiz = newScore;
+}
+void MAS3114::updateExams(int examNumber, double newScore) {
+	exams[examNumber] = newScore;
+}
+//this is a generic thing that adds up the contents of an array and returns the value
+double MAS3114::pointSummer(double grades[], int arraySize) {
+	double total = 0;
+	for (int i = 0; i < arraySize; i++) {
+		//this check makes it so only grades that have been entered are added to the total
+		if (grades[i] != -1) {
+			total += grades[i];
+		}
+	}
+	return total;
+}
+double MAS3114::examPointSummer(array<double, 20> examGrades) {
+	double total = 0;
+	for (int i = 0; i < examGrades.size(); i++) {
+		//this check makes it so only grades that have been entered are added to the total
+		if (examGrades[i] != -1) {
+			total += examGrades[i];
+		}
+	}
+	return total;
+}
 //This method condenses all the points and then sets the gpa value accordingly
-void MAS3114::calcGPA() {
-	double totalPoints;
-	totalPoints += pointSummer(homework);
-	totalPoints += pointSummer(projects);
-	totalPoints += pointSummer(exams);
-	totalPoints += participation;
-	totalPoints += quiz;
-
+void MAS3114::calcGpa() {
+	double totalPoints = 0;
+	double divider = 0;
+	//count number of homeworks entered
+	int homeworkCount = 0;
+	for (int i = 0; i < 30; i++) {
+		if (homework[i] != -1) {
+			homeworkCount++;
+		}
+	}
+	if (homeworkCount > 0) {
+		totalPoints += ((pointSummer(homework, 30) / homeworkCount) * 0.9);
+		divider += 90;
+	}
+	//count number of projects entered
+	int projectCount = 0;
+	for (int i = 0; i < 5; i++) {
+		if (projects[i] != -1) {
+			projectCount++;
+		}
+	}
+	if (projectCount > 0) {
+		totalPoints += ((pointSummer(projects, 5) / projectCount) * 1.3);
+		divider += 130;
+	}
+	//count number of exams entered
+	int examCount = 0;
+	for (int i = 0; i < 20; i++) {
+		if (exams[i] != -1) {
+			examCount++;
+		}
+	}
+	if (examCount > 0) {
+		totalPoints += ((examPointSummer(exams) / examCount) * 2.4);
+		divider += 240;
+	}
+	if (participation != -1) {
+		totalPoints += (participation * 0.525);
+		divider += 52.5;
+	}
+	if (quiz != -1) {
+		totalPoints += (quiz * 0.6);
+		divider += 60;
+	}
+	double percentage;
+	if (divider != 0) {
+		percentage = (totalPoints / divider) * 100;
+	}
+	else {
+		percentage = -1;
+	}
 	//these ifs serve to set the gpa according to the total point value
 	//the syllabus based the final grade on percentage so I converted it for the checks
-	double percentage = (totalPoints / 572.5) * 100;
-
 	if (percentage >= 90) {
 		gpa = 4.0;
 	}
@@ -47,33 +142,10 @@ void MAS3114::calcGPA() {
 	else if (percentage >= 50 && percentage < 54) {
 		gpa = 0.67;
 	}
-	else {
+	else if (percentage >= 0 && percentage < 50) {
 		gpa = 0;
 	}
-}
-//these methods take in a new grade value and change or set the existing value
-//may need this-> pointer, not sure because I can't test the functionality
-//if it does, it would be redone like this: MAS3114->finalExam = newScore;
-void MAS3114::updateHomework(int homeworkNumber, double newScore) {
-	homework[homeworkNumber] = newScore;
-}
-void MAS3114::updateProjects(int projectNumber, double newScore) {
-	projects[projectNumber] = newscore;
-}
-void MAS3114::updateParticipation(double newScore) {
-	participation = newScore;
-}
-void MAS3114::updateQuiz(double newScore) {
-	quiz = newScore;
-}
-void MAS3114::updateExams(int examNumber, double newScore) {
-	exams[examNumber] = newScore;
-}
-//this is a generic thing that adds up the contents of an array and returns the value
-double pointSummer(std::array grades) {
-	double total = 0;
-	for (int i = 0; i < grades.size(); i++) {
-		total += grades[i];
+	else {
+		gpa = -1;
 	}
-	return total;
 }
