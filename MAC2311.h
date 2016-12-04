894160370fea7d368315782b6f62a5ae2fb0b3ec
redgreene <redgreenenaoh@gmail.com>
2016-12-04 01:00:02 -0500

    Update MAC2311.h

--- MAC2311.h
+++ MAC2311.h
@@ -1,49 +1,33 @@
-#include <array>
+#include "Course.cpp"
+#ifndef _MAC2311_H_
+#define _MAC2311_H_
+
+//the ifndef and define stuff was a bug fix relating to something called a header guard
 
 //this bit says that this particular class is a child of Course
-class MAC2311 : private Course { //not sure if private is the thing to use here
+class MAC2311 : public Course { 
 private:
 	//NOTE: Exams and Final in this course are out of 75 points each
 	//Course grade is based on 450 available points
 	//Final grade can replace lowest exam grade if it is better
 	double hittPoints; //40 points
-	std::array <double, 13> webAssign; // total maxes at 50 points
-	std::array <double, 10> quizzes; //best 8 of 10, 6 points each
-	std::array <double, 5> writtenHomework; //best 4 of 5, 3 points each
-	
-	void calcGPA() {
-		
-	}
+	double webAssign[13]; // total maxes at 50 points, enter these grades as percentages
+	double quizzes[10]; //best 8 of 10, 6 points each
+	double writtenHomework[5]; //best 4 of 5, 3 points each
 
 public:
-	void updateHittPoints(double) {
-
-	}
-	void updateWebAssign(int, double) {
-
-	}
-	void updateQuiz(int, double) {
-
-	}
-	void updateWrittenHomework(int, double) {
-
-	}
-	void updateExam(int, double) {
-
-	}
-	void updateFinal(double) {
-
-	}
-	void examReplace(std::array, double) {
-
-	}
-	double pointSummer(std::array) {
-
-	}
-	double bestOfQuizzes(std::array) {
-
-	}
-	double bestOfWrittenHomework(std::array) {
-
-	}
-}
+	MAC2311::MAC2311();
+	void calcGPA();
+	void updateHittPoints(double);
+	void updateWebAssign(int, double);
+	void updateQuiz(int, double);
+	void updateWrittenHomework(int, double);
+	void updateExam(int, double);
+	void updateFinal(double);
+	void examReplace(double[], double);
+	double pointSummer(double[], int);
+	double bestOfQuizzes(double[]);
+	double bestOfWrittenHomework(double[]);
+	double webAssignCalculator(double[]);
+};
+#endif
