29711471b5f3d8e86d7458b396cac9db7f45e9c4
redgreene <redgreenenaoh@gmail.com>
2016-12-04 02:28:05 -0500

    Update MAC2312.cpp

--- MAC2312.cpp
+++ MAC2312.cpp
@@ -1,63 +1,22 @@
-#include "stdafx.h"
 #include "MAC2312.h"
 
-//This method condenses all the points and then sets the gpa value accordingly
-void MAC2312::calcGPA() {
-	double totalPoints = 0;
-	totalPoints += hittPoints;
-	//webAssign points max out at 50
-	if (webAssign > 50) {
-		totalpoints += 50;
-	}
-	else {
-		totalPoints += webAssign;
-	}
-	totalPoints += bestOf(quizzes);
-	totalPoints += pointSummer(writtenHomework);
-	totalPoints += participation;
-	totalPoints += pointSummer(exams);
-	totalPoints += finalExam; //assuming final exam grade will be named finalExam instead of just final
-	//these ifs serve to set the gpa according to the total point value
-	if (totalPoints >= 360) {
-		gpa = 4;
-	}
-	else if (totalPoints >= 348 && totalPoints <= 459) {
-		gpa = 3.67;
-	}
-	else if (totalPoints >= 334 && totalPoints <= 347) {
-		gpa = 3.33;
-	}
-	else if (totalPoints >= 320 && totalPoints <= 333) {
-		gpa = 3;
-	}
-	else if (totalPoints >= 308 && totalPoints <= 319) {
-		gpa = 2.67;
-	}
-	else if (totalPoints >= 294 && totalPoints <= 307) {
-		gpa = 2.33;
-	}
-	else if (totalPoints >= 280 && totalPoints <= 293) {
-		gpa = 2;
-	}
-	else if (totalPoints >= 268 && totalPoints <= 279) {
-		gpa = 1.67;
-	}
-	else if (totalPoints >= 254 && totalPoints <= 267) {
-		gpa = 1.33;
-	}
-	else if (totalPoints >= 240 && totalPoints <= 253) {
-		gpa = 1;
-	}
-	else if (totalPoints >= 228 && totalPoints <= 239) {
-		gpa = 0.67;
-	}
-	else {
-		gpa = 0;
+//constructor initializes everything to -1
+MAC2312::MAC2312() {
+	hittPoints = -1;
+	participation = -1;
+	webAssign = -1;
+	finals = -1;
+	for (int i = 0; i < 20; i++) {
+		if (i < 8) {
+			quizzes[i] = -1;
+		}
+		if (i < 3) {
+			writtenHomework[i] = -1;
+		}
+		exams[i] = -1;
 	}
 }
-//these methods take in a new grade value and change or set the existing value
-//may need this-> pointer, not sure because I can't test the functionality
-//if it does, it would be redone like this: MAC2312->finalExam = newScore;
+//these methods take in a new grade value and change the existing value
 void MAC2312::updateHittPoints(double newScore) {
 	hittPoints = newScore;
 }
@@ -77,29 +36,160 @@ void MAC2312::updateExam(int examNumber, double newScore) {
 	exams[examNumber] = newScore;
 }
 void MAC2312::updateFinal(double newScore) {
-	finalExam = newScore;
+	finals = newScore;
 }
 //this is a generic thing that adds up the contents of an array and returns the value
-double pointSummer(std::array grades) {
+double MAC2312::pointSummer(double grades[], int arraySize) {
 	double total = 0;
-	for (int i = 0; i < grades.size(); i++) {
-		total += grades[i];
+	for (int i = 0; i < arraySize; i++) {
+		//this check makes it so only grades that have been entered are added to the total
+		if (grades[i] != -1) {
+			total += grades[i];
+		}
 	}
 	return total;
 }
 //this method sorts through the quiz array and finds the index of the two lowest quiz grades
 //these two grades are dropped
 //and then the rest of the grades are summed and returned as a double
-double bestOfQuizzes(std::array quizzes) {
+double MAC2312::bestOfQuizzes(double quizzes[]) {
 	double bestSum = 0;
 	int lowestIndex = 0;
-	int nextLowest = 0;
-	for (int i = 1; i < 10; i++) {
+	//finding lowest index
+	for (int i = 1; i < 8; i++) {
 		if (quizzes[i] < quizzes[lowestIndex]) {
-			nextLowest = lowestIndex;
 			lowestIndex = i;
 		}
 	}
-	bestSum = pointSummer(quizzes) - quizzes[lowestIndex] - quizzes[nextLowest];
+	int nextLowest;
+	if (lowestIndex == 0) {
+		nextLowest = 1;
+	}
+	else {
+		nextLowest = 0;
+	}
+	//finds 2nd lowest by redoing the check and ignoring the lowest one
+	for (int i = 1; i < 8; i++) {
+		if (quizzes[i] < quizzes[nextLowest] && i != lowestIndex) {
+			nextLowest = i;
+		}
+	}
+	bestSum = pointSummer(quizzes, 8) - quizzes[lowestIndex] - quizzes[nextLowest];
 	return bestSum;
+}
+//This method condenses all the points and then sets the gpa value accordingly
+void MAC2312::calcGPA() {
+	double totalPoints = 0; //this tracks number of points earned
+	double pointDivision = 0; //this will track number of available points based on what has been graded
+	if (hittPoints != -1) {
+		totalPoints += hittPoints;
+		pointDivision += 35;
+	}
+	if (participation != -1) {
+		totalPoints += participation;
+		pointDivision += 4;
+	}
+	//webAssign points max out at 50
+	if (webAssign != -1) {
+		if (webAssign > 50) {
+			totalPoints += 50;
+		}
+		else {
+			totalPoints += webAssign;
+		}
+		pointDivision += 50;
+	}
+	//now figure out how many quiz grades have been entered
+	int quizCount = 0;
+	for (int i = 0; i < 10; i++) {
+		if (quizzes[i] != -1) {
+			quizCount++;
+		}
+	}
+	//the if determinse how the quizzes affect the grade
+	//because there were a few different cases
+	if (quizCount > 6) {
+		pointDivision += 36;
+		totalPoints += bestOfQuizzes(quizzes);
+	}
+	else if (quizCount > 0 && quizCount < 6) {
+		pointDivision += (6 * quizCount);
+		totalPoints += pointSummer(quizzes, 8);
+	}
+	//count entered written homework grades
+	int writtenHomeworkCount = 0;
+	for (int i = 0; i < 3; i++) {
+		if (writtenHomework[i] != -1) {
+			writtenHomeworkCount++;
+		}
+	}
+	if (writtenHomeworkCount > 0) {
+		pointDivision += (5 * writtenHomeworkCount);
+		totalPoints += pointSummer(writtenHomework, 3);
+	}
+	//count entered exam grades
+	int examCount = 0;
+	for (int i = 0; i < 3; i++) {
+		if (exams[i] != -1) {
+			examCount++;
+		}
+	}
+	if (examCount > 0) {
+		pointDivision += (60 * examCount);
+		totalPoints += pointSummer(exams, 20);
+	}
+	//final exam
+	if (finals != -1) {
+		pointDivision += 80;
+		totalPoints += finals;
+	}
+	//makes sure no division by zer0 when finding percentage
+	double percentage;
+	if (pointDivision != 0) {
+		percentage = (totalPoints / pointDivision);
+	}
+	else {
+		//if no grades are in, gpa returns -1, which is what a percentage of -1 will lead to
+		percentage = -1;
+	}
+	//these ifs serve to set the gpa according to the total point value
+	if (percentage >= (360.0 / 400.0)) {
+		setGpa(4);
+	}
+	else if (percentage >= (348.0 / 400.0) && totalPoints < (360.0 / 400.0)) {
+		setGpa(3.67);
+	}
+	else if (percentage >= (334.0 / 400.0) && percentage < (348.0 / 400.0)) {
+		setGpa(3.33);
+	}
+	else if (percentage >= (320.0 / 400.0) && percentage < (334.0 / 400.0)) {
+		setGpa(3);
+	}
+	else if (percentage >= (308.0 / 400.0) && percentage < (320.0 / 400.0)) {
+		setGpa(2.67);
+	}
+	else if (percentage >= (294.0 / 400.0) && percentage < (308.0 / 400.0)) {
+		setGpa(2.33);
+	}
+	else if (percentage >= (280.0 / 400.0) && percentage < (294.0 / 400.0)) {
+		setGpa(2);
+	}
+	else if (percentage >= (268.0 / 400.0) && percentage < (280.0 / 400.0)) {
+		setGpa(1.67);
+	}
+	else if (percentage >= (254.0 / 400.0) && percentage < (268.0 / 400.0)) {
+		setGpa(1.33);
+	}
+	else if (percentage >= (240.0 / 400.0) && percentage < (254.0 / 400.0)) {
+		setGpa(1);
+	}
+	else if (percentage >= (228.0 / 400.0) && percentage < (240.0 / 400.0)) {
+		setGpa(0.67);
+	}
+	else if (percentage >= 0 && percentage < (228.0 / 400.0)) {
+		setGpa(0);
+	}
+	else {
+		gpa = -1;
+	}
 }
