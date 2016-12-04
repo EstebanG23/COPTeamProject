3784570fc0ed0514f6e042e3bd111cd135fea63a
redgreene <redgreenenaoh@gmail.com>
2016-12-04 17:05:26 -0500

    Update COP3503.h

--- COP3503.h
+++ COP3503.h
@@ -1,29 +1,26 @@
+#pragma once
+#include "Course.h"
 #include <array>
+#include <iostream>
+using namespace std;
 
 //this bit says that this particular class is a child of Course
-class COP3503 : private Course { //not sure if private is the thing to use here
+class COP3503 : public Course { 
 private:
 	//This course is percentage based, so scores will need to be entered by percentage
 	//Example, if they get a 79% on the first exam, enter 79
 	//not that complicated, but important distinction because other courses do a total sum of points
 	//2 exams, no final
 	double groupProject;
-	std::array <double, 3> assignments;
+	double assignments[3];
+	array <double, 3> tempAssignments;
 
-	void calcGpa() {
-
-	}
 public:
-	void COP3503::updateAssignments(int, double) {
-
-	}
-	void COP3503::updateGroupProject(double) {
-
-	}
-	void COP3503::updateExams(int, double) {
-
-	}
-	double pointSummer(std::array) {
-
-	}
+	COP3503();
+	void COP3503::calcGpa();
+	void COP3503::updateAssignments(int, double);
+	void COP3503::updateGroupProject(double);
+	void COP3503::updateExams(int, double);
+	double COP3503::pointSummer(double[], int);
+	double COP3503::examPointSummer(array<double, 20>);
 };
